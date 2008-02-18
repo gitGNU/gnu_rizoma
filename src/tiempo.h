8c870366a8dd1701a15402b0fedfbb97f5b8f430
Jonathan Gonzalez V <jonathan@rizoma.cl>
2008-02-18 18:20:09 +0000

    Correccion de algunos license notice

--- src/tiempo.h
+++ src/tiempo.h
@@ -2,7 +2,9 @@
 *
 *    Copyright (C) 2004 Rizoma Tecnologia Limitada <info@rizoma.cl>
 *
-*       This program is free software; you can redistribute it and/or modify
+*    This file is part of rizoma.
+*
+*    Rizoma is free software; you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
 *    the Free Software Foundation; either version 2 of the License, or
 *    (at your option) any later version.
