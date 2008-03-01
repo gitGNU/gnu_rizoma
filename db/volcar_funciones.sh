90035ef8caa22ce87524a2a0074f9f836dd9e542
Jonathan Gonzalez V <zeus@gnu.org>
2008-03-01 20:45:21 -0300

    Add the file volcar_funciones.sh to dump al pgpsql functions to a defined db

---
+++ db/volcar_funciones.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+##
+# Copyright (C) 2008 Rizoma Tecnologia Limitada <info@rizoma.cl>
+
+# This file is part of rizoma.
+
+# Rizoma is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+DB_NAME=$1
+DB_USER=$2
+DB_HOST=$3
+DB_PORT=$4
+
+if [ -z $1 ] ; then
+    echo "* Usage : volcar_funciones.sh db_name db_user db_host db_port"
+    exit -1
+fi
+
+echo "$DB_HOST:$DB_PORT:$DB_NAME:$DB_USER:" > ~/.pgpass
+chmod 0600 ~/.pgpass
+
+psq $DB_NAME < `psql rizoma -c "\df public." | grep '|' | grep 'public' | awk -F'|' '{print "DROP FUNCTION" $2 "("$4");"}'`
+
+psql $DB_NAME < funciones.sql
+
+rm ~/.pgpass
+
