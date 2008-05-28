d919f4fbd8cb29e48cdfb16c89845feebf1cf61b
Felipe Reyes <felipereyes@gmail.com>
2008-05-28 19:27:30 -0400

    added admin.c admin.h that is the start point of rizoma-admin

---
+++ src/admin.c
@@ -0,0 +1,196 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4;
+       c-indentation-style: gnu -*- */
+/*admin.c
+ *
+ *    Copyright (C) 2008 Rizoma Tecnologia Limitada <info@rizoma.cl>
+ *
+ *    This file is part of rizoma.
+ *
+ *    Rizoma is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include<gtk/gtk.h>
+#include<stdlib.h>
+
+#include"../config.h"
+
+#include"admin.h"
+#include"tipos.h"
+#include"usuario.h"
+#include"config_file.h"
+#include"postgres-functions.h"
+#include"encriptar.h"
+#include"rizoma_errors.h"
+
+//the builder that contains all the UI
+GtkBuilder *builder;
+
+
+int
+main (int argc, char **argv)
+{
+  GtkWindow *login_window;
+  GError *err = NULL;
+
+  GtkComboBox *combo;
+  GtkListStore *model;
+  GtkTreeIter iter;
+  GtkCellRenderer *cell;
+
+  char *config_file;
+  GKeyFile *key_file;
+  gchar **profiles;
+
+  config_file = g_strconcat(g_getenv("HOME"),"/.rizoma", NULL);
+
+  if (!g_file_test(config_file,
+                   G_FILE_TEST_EXISTS|G_FILE_TEST_IS_REGULAR))
+    {
+      perror (g_strdup_printf ("Opening %s", config_file));
+      printf ("Para configurar su sistema debe ejecutar rizoma-config\n");
+      exit (-1);
+    }
+  key_file = g_key_file_new ();
+  g_key_file_load_from_file(key_file, config_file, G_KEY_FILE_KEEP_COMMENTS, NULL);
+
+  gtk_init (&argc, &argv);
+
+  builder = gtk_builder_new ();
+
+  gtk_builder_add_from_file (builder, DATADIR"/ui/rizoma-login.ui", &err);
+  if (err) {
+    g_error ("ERROR: %s\n", err->message);
+    return -1;
+  }
+
+  gtk_builder_connect_signals (builder, NULL);
+
+  login_window = GTK_WINDOW(gtk_builder_get_object (builder, "login_window"));
+
+  profiles = g_key_file_get_groups (key_file, NULL);
+  g_key_file_free (key_file);
+
+  model = gtk_list_store_new (1,
+                              G_TYPE_STRING);
+
+  combo = (GtkComboBox *) gtk_builder_get_object (builder, "combo_profile");
+
+  cell = gtk_cell_renderer_text_new ();
+  gtk_cell_layout_pack_start (GTK_CELL_LAYOUT(combo), cell, TRUE);
+  gtk_cell_layout_set_attributes (GTK_CELL_LAYOUT(combo), cell,
+                                  "text", 0,
+                                  NULL);
+  do
+    {
+      if (*profiles != NULL)
+        {
+          gtk_list_store_append (model, &iter);
+          gtk_list_store_set (model, &iter,
+                              0, *profiles,
+                              -1
+                              );
+        }
+    } while (*profiles++ != NULL);
+
+  gtk_combo_box_set_model (combo, (GtkTreeModel *)model);
+  gtk_combo_box_set_active (combo, 0);
+
+  gtk_widget_show_all ((GtkWidget *)login_window);
+
+  gtk_main();
+
+  return 0;
+}
+
+void
+check_passwd (GtkWidget *widget, gpointer data)
+{
+  GtkTreeIter iter;
+  GtkComboBox *combo = (GtkComboBox *) gtk_builder_get_object (builder, "combo_profile");
+  GtkTreeModel *model = gtk_combo_box_get_model (combo);
+  gchar *group_name;
+
+  gchar *passwd = g_strdup (gtk_entry_get_text ( (GtkEntry *) gtk_builder_get_object (builder,"passwd_entry")));
+  gchar *user = g_strdup (gtk_entry_get_text ( (GtkEntry *) gtk_builder_get_object (builder,"user_entry")));
+
+  gtk_combo_box_get_active_iter (combo, &iter);
+  gtk_tree_model_get (model, &iter,
+                      0, &group_name,
+                      -1);
+
+  rizoma_set_profile (group_name);
+
+  switch (AcceptPassword (passwd, user))
+    {
+    case TRUE:
+
+      user_data = (User *) g_malloc (sizeof (User));
+
+      user_data->user_id = ReturnUserId (user);
+      user_data->level = ReturnUserLevel (user);
+      user_data->user = user;
+
+      gtk_widget_destroy (GTK_WIDGET(gtk_builder_get_object (builder,"login_window")));
+      g_object_unref ((gpointer) builder);
+      builder = NULL;
+
+      admin_win();
+
+      break;
+    case FALSE:
+      gtk_entry_set_text ((GtkEntry *) gtk_builder_get_object (builder,"user_entry"), "");
+      gtk_entry_set_text ((GtkEntry *) gtk_builder_get_object (builder,"passwd_entry"), "");
+      rizoma_error_window ((GtkWidget *) gtk_builder_get_object (builder,"user_entry"));
+      break;
+    default:
+      break;
+    }
+}
+
+void
+admin_win()
+{
+  GtkWidget *admin_gui;
+  GError *error;
+
+  builder = gtk_builder_new ();
+
+  gtk_builder_add_from_file (builder, DATADIR"/ui/rizoma-admin.ui", &error);
+
+  if (error != NULL) {
+    g_printerr ("%s: %s\n", G_STRFUNC, error->message);
+  }
+
+  gtk_builder_add_from_file (builder, DATADIR"/ui/rizoma-common.ui", &error);
+
+  if (error != NULL) {
+    g_printerr ("%s: %s\n", G_STRFUNC, error->message);
+  }
+
+  gtk_builder_connect_signals (builder, NULL);
+
+  admin_gui = GTK_WIDGET (gtk_builder_get_object (builder, "wnd_admin"));
+
+  // check if the window must be set to fullscreen
+  if (rizoma_get_value_boolean("FULLSCREEN"))
+    gtk_window_fullscreen(GTK_WINDOW(admin_gui));
+
+  //setup the different tabs
+  user_box(); //users tab
+  taxes_box(); //taxes tab
+  //business_box();
+  //preferences_box();
+  gtk_widget_show_all (admin_gui);
+}
