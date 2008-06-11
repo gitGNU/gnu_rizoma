72c59a8aff2d922d682be201a4bf812a92941dd3
Jonathan Gonzalez V <zeus@gnu.org>
2008-06-11 17:43:35 -0400

    Deleted tabs and re-indent

--- src/tiempo.c
+++ src/tiempo.c
@@ -1,25 +1,25 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4;
-       c-indentation-style: gnu -*- */
+   c-indentation-style: gnu -*- */
 /*tiempo.c
-*
-*    Copyright (C) 2004 Rizoma Tecnologia Limitada <info@rizoma.cl>
-*
-*    This file is part of rizoma.
-*
-*    Rizoma is free software; you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation; either version 2 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program; if not, write to the Free Software
-*    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ *
+ *    Copyright (C) 2004,2008 Rizoma Tecnologia Limitada <info@rizoma.cl>
+ *
+ *    This file is part of rizoma.
+ *
+ *    Rizoma is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #include<gtk/gtk.h>
 
@@ -79,9 +79,9 @@ show_date (void)
 
   date = gtk_label_new ("");
   gtk_label_set_markup (GTK_LABEL (date),
-			g_strdup_printf ("<span size=\"xx-large\"><b>%s %d\n%s\n%d</b></span>",
-					 days[fecha->tm_wday].name, fecha->tm_mday,
-					 months[fecha->tm_mon].name, YEAR (fecha->tm_year)));
+                        g_strdup_printf ("<span size=\"xx-large\"><b>%s %d\n%s\n%d</b></span>",
+                                         days[fecha->tm_wday].name, fecha->tm_mday,
+                                         months[fecha->tm_mon].name, YEAR (fecha->tm_year)));
 
   gtk_label_set_justify (GTK_LABEL (date), GTK_JUSTIFY_CENTER);
 
@@ -91,33 +91,6 @@ show_date (void)
 
   return date;
 }
-
-gboolean
-RefreshTime (gpointer data)
-{
-  time_t t;
-  struct tm *hora;
-
-  time (&t);
-
-  hora = localtime (&t);
-
-
-  gtk_label_set_markup (GTK_LABEL (hour_label),
-			g_strdup_printf ("<span size=\"xx-large\"><b>%2.2d:%2.2d:%2.2d</b></span>",
-					 hora->tm_hour, hora->tm_min, hora->tm_sec));
-
-  if (hora->tm_mday != day || hora->tm_mon != month || hora->tm_year != year)
-    {
-      gtk_label_set_markup (GTK_LABEL (date_label),
-			    g_strdup_printf ("<span size=\"xx-large\"><b>%s %d\n%s\n%d</b></span>",
-					     days[hora->tm_wday].name, hora->tm_mday,
-					     months[hora->tm_mon].name, YEAR (hora->tm_year)));
-    }
-
-  return TRUE;
-}
-
 gchar *
 CurrentDate (void)
 {
