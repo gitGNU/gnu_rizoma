5b6fb00eafe6527d4746e98cfafa8d59321d7880
Felipe Reyes <felipereyes@gmail.com>
2008-06-12 12:36:50 -0400

    [errors] ExitoMSG() now supports markup message in the pango style
    see Pango's text markup language in the gtk documentation

--- src/errors.c
+++ src/errors.c
@@ -211,6 +211,7 @@ ExitoMSG (GtkWidget *widget, gchar *motivo)
     gtk_box_pack_start (GTK_BOX (hbox), image, FALSE, FALSE, 3);
 
     label = gtk_label_new (motivo);
+    gtk_label_set_use_markup (GTK_LABEL(label), TRUE);
     gtk_widget_show (label);
     gtk_box_pack_start (GTK_BOX (hbox), label, FALSE, FALSE, 3);
 
