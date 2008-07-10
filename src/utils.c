7b7803e51f64e69878ff0d88bbba4860fad1c362
Jonathan Gonzalez V <zeus@gnu.org>
2008-07-10 15:39:48 -0400

    [src] Fixed bug that was deleting labels of any widget inside a container even those inside a stock button or radio button.

--- src/utils.c
+++ src/utils.c
@@ -342,7 +342,7 @@ clean_container (GtkContainer *container)
         {
           widget = GTK_WIDGET (list->data);
           widget_name = g_strdup (gtk_widget_get_name (widget));
-          if (!validate_string ("[0-9]+", widget_name))
+          if (!validate_string ("[0-9]+", widget_name) && !validate_string ("Gtk", widget_name))
             {
               if (GTK_IS_ENTRY (widget))
                 {
