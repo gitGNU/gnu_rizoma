b51cde4f25e6a3ce875d40197c7af28fe2856d5e
Carlos Contreras Bolton <ccontreras.bolton@gmail.com>
2010-06-05 17:28:10 -0400

    Fixed prepareChangelog and update ChangeLog

--- prepareChangelog.py
+++ prepareChangelog.py
@@ -23,59 +23,126 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##
 
-import sys
-import os
-import time
-import tempfile
+# Copyright 2008 Marcus D. Hanwell <marcus@cryos.org>
+# Distributed under the terms of the GNU General Public License v2 or later
 
-CHANGELOG = 'ChangeLog'
+import string, re, os
 
-f_status = os.popen('git-diff-index --cached HEAD', 'r')
+# Execute git log with the desired command line options.
+fin = os.popen('git log --summary --stat --no-merges --date=short', 'r')
+# Create a ChangeLog file in the current directory.
+fout = open('ChangeLog', 'w')
 
-curr_date = time.localtime(time.time())
-change_date = change_date = str(curr_date[0]) + '-' +\
-              str('%.2d' % curr_date[1]) + ' ' +  \
-              str('%.2d' % curr_date[2])
+# Set up the loop variables in order to locate the blocks we want
+authorFound = False
+dateFound = False
+messageFound = False
+filesFound = False
+message = ""
+messageNL = False
+files = ""
+prevAuthorLine = ""
 
-f_author_name = os.popen('git config --get user.name').readline()
-author_name = f_author_name[:len(f_author_name)-1]
+# The main part of the loop
+for line in fin:
+    # The commit line marks the start of a new commit object.
+    if string.find(line, 'commit') >= 0:
+        # Start all over again...
+        authorFound = False
+        dateFound = False
+        messageFound = False
+        messageNL = False
+        message = ""
+        filesFound = False
+        files = ""
+        continue
+    # Match the author line and extract the part we want
+    elif re.match('Author:', line) >=0:
+        authorList = re.split(': ', line, 1)
+        author = authorList[1]
+        author = author[0:len(author)-1]
+        authorFound = True
+    # Match the date line
+    elif re.match('Date:', line) >= 0:
+        dateList = re.split(':   ', line, 1)
+        date = dateList[1]
+        date = date[0:len(date)-1]
+        dateFound = True
+    # The svn-id lines are ignored
+    elif re.match('    git-svn-id:', line) >= 0:
+        continue
+    # The sign off line is ignored too
+    elif re.search('Signed-off-by', line) >= 0:
+        continue
+    # Extract the actual commit message for this commit
+    elif authorFound & dateFound & messageFound == False:
+        # Find the commit message if we can
+        if len(line) == 1:
+            if messageNL:
+                messageFound = True
+            else:
+                messageNL = True
+        elif len(line) == 4:
+            messageFound = True
+        else:
+            if len(message) == 0:
+                message = message + line.strip()
+            else:
+                message = message + " " + line.strip()
+    # If this line is hit all of the files have been stored for this commit
+    elif re.search('files changed', line) >= 0:
+        filesFound = True
+        continue
+    # Collect the files for this commit. FIXME: Still need to add +/- to files
+    elif authorFound & dateFound & messageFound:
+        fileList = re.split(' \| ', line, 2)
+        if len(fileList) > 1:
+            if len(files) > 0:
+                files = files + ", " + fileList[0].strip()
+            else:
+                files = fileList[0].strip()
+    # All of the parts of the commit have been found - write out the entry
+    if authorFound & dateFound & messageFound & filesFound:
+        # First the author line, only outputted if it is the first for that
+        # author on this day
+        authorLine = date + "  " + author
+        if len(prevAuthorLine) == 0:
+            fout.write(authorLine + "\n")
+        elif authorLine == prevAuthorLine:
+            pass
+        else:
+            fout.write("\n" + authorLine + "\n")
 
-f_author_email = os.popen('git config --get user.email').readline()
-author_email = f_author_email[:len(f_author_email)-1]
+        # Assemble the actual commit message line(s) and limit the line length
+        # to 80 characters.
+        commitLine = "* " + files + ": " + message
+        i = 0
+        commit = ""
+        while i < len(commitLine):
+            if len(commitLine) < i + 78:
+                commit = commit + "\n  " + commitLine[i:len(commitLine)]
+                break
+            index = commitLine.rfind(' ', i, i+78)
+            if index > i:
+                commit = commit + "\n  " + commitLine[i:index]
+                i = index+1
+            else:
+                commit = commit + "\n  " + commitLine[i:78]
+                i = i+79
 
-modified_files = list()
+        # Write out the commit line
+        fout.write(commit + "\n")
 
-for line in f_status.readlines():
-    l = line.split('\t')
-    f_file = l[len(l)-1]
-    f_file = f_file[:len(f_file)-1]
-    modified_files.append(f_file)
+        #Now reset all the variables ready for a new commit block.
+        authorFound = False
+        dateFound = False
+        messageFound = False
+        messageNL = False
+        message = ""
+        filesFound = False
+        files = ""
+        prevAuthorLine = authorLine
 
-if len(l) == 0:
-    print "There is no files waiting for commit"
-    sys.exit(-1)
-
-header = change_date + "  " + author_name + "  <" + author_email + ">"
-header += "\n\n"
-
-for l in modified_files:
-    header +="\t* " + l + ": \n"
-
-header += "\n"
-
-tmp_dir = tempfile.mkdtemp()
-os.system('cp ' + CHANGELOG + ' ' + tmp_dir)
-
-changelog_file = open(CHANGELOG,'w')
-changelog_file.write(header)
-
-tmp_file = open(tmp_dir + "/" + CHANGELOG, 'r')
-
-for l in tmp_file.readlines():
-    changelog_file.write(l)
-
-tmp_file.close()
-changelog_file.close()
-os.system('rm -rf ' + tmp_dir)
-
-os.system("$EDITOR " + CHANGELOG)
+# Close the input and output lines now that we are finished.
+fin.close()
+fout.close()
